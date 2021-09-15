80eba449035176bd6d06b94bf356ab30b8e4d389
Jörg Thalheim <joerg@thalheim.io>
2021-09-15 10:03:18 +0200

    switch to fenix from rustup

--- default.nix
+++ default.nix
@@ -1,8 +1,16 @@
 with import <nixpkgs> {};
+let
+  fenixRev = "e917dae2552d6d50797a718bffba34102af082bf";
+  fenix = pkgs.callPackage (fetchTarball "https://github.com/nix-community/fenix/archive/${fenixRev}.tar.gz") { };
+in
 mkShell {
   nativeBuildInputs = [
     bashInteractive
-    rustup
+    (fenix.combine [
+      fenix.minimal.rustc
+      fenix.minimal.cargo
+      fenix.targets.wasm32-unknown-unknown.latest.rust-std
+    ])
     wasm-bindgen-cli
     trunk
   ];
